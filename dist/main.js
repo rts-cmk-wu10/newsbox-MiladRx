4d5ce32e136e7304bc8db71fe62c48a953351123
MiladRx <miladroubei70@gmail.com>
2023-11-02 13:53:21 +0100

    added app.js with API

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,18 @@
 /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
 var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/scripts/app.js
+/* harmony default export */ var app = ((function() {
+fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=F7Q16NruiZVUiGVH934NpdQgCBkkIz2s')
+  .then(response => response.json())
+  .then(data => {
+   
+    console.log(data);
+  });
+})());
+;// CONCATENATED MODULE: ./src/index.js
 
 /******/ })()
 ;
+//# sourceMappingURL=main.js.map
